ffb1194dff1efc38963cdc98525b4b2154100c34
Nairinhe <nairinhe@gmail.com>
2022-07-08 23:08:37 +0300

    eso plus trial https://www.elderscrollsonline.com/en-us/news/post/62459

--- scripts/releases_list.js
+++ scripts/releases_list.js
@@ -149,6 +149,7 @@ var events =
 		{key: "EsoPlusTrial",		datestart: new Date(2021, 0, 19)},
 		{key: "EsoPlusTrial",		datestart: new Date(2021, 5, 14)},
 		{key: "EsoPlusTrial",		datestart: new Date(2021, 9, 18)},
+		{key: "EsoPlusTrial",		datestart: new Date(2022, 6, 7)},
 
 		{key: "FreePlay",		datestart: new Date(2017, 3, 11)},
 		{key: "FreePlay",		datestart: new Date(2017, 10, 30)},
